c24fa35cac29a3b895b85a5ee37660d3d794f5a0
HaithemGhattass <haithem.ghattas@esprit.tn>
2025-12-23 13:16:30 +0100

    draft:drafting App static

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
+import Work from "./containers/Work";
+import Stack from "./containers/Stack";
+import About from "./containers/About";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mesh-bg text-slate-800 antialiased selection:bg-blue-100 selection:text-blue-700">
+      <Nav />
+      <main class="pt-32 pb-20 px-6 max-w-7xl mx-auto">
+        <Hero />
+        <Work />
+        <Stack />
+        <About />
+        <Footer />
+      </main>
     </div>
   );
 }
